af5d5caad6cd22286720bccded58b44ef9e653a7
mdabezies <dabeziesmaria@gmail.com>
2024-08-12 20:50:01 -0300

    Change screensize for timer changes and remove ceremony public transport info

--- src/components/PhotosQR/PhotosQR.jsx
+++ src/components/PhotosQR/PhotosQR.jsx
@@ -3,10 +3,10 @@ import strings from "../../strings/phtos-qr.json";
 const PhotosQR = () => {
     
     return strings.show ? (
-        <section className="text-center flex flex-col items-center gap-10 w-full bg-jacarta-300 text-neutral-cream-100 py-12">
+        <section className="text-center flex flex-col items-center gap-10 w-full bg-jacarta-300 text-neutral-cream-100 py-12  px-6 md:px-32 lg:px-40">
             <p className="text-5xl font-serif font-bold">{strings.title}</p>
-            <p className="lg:px-40">{strings.subtitle}</p>
-            <a href={strings.link} target="blank" className="w-[90dvw] md:w-[80dvw] lg:w-[20dvw] mx-auto hover:scale-105 transition-all">
+            <p className="">{strings.subtitle}</p>
+            <a href={strings.link} target="blank" className="w-[50dvw] md:w-[40dvw] lg:w-[20dvw] mx-auto hover:scale-105 transition-all">
                 <img src="/images/QR.png" alt="QR to pictures link" className="rounded-md" />
             </a>
         </section>
