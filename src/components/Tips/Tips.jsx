76ad9843c11ca20ed9ac24049eeee24a29dc88f9
mdabezies <dabeziesmaria@gmail.com>
2024-08-16 20:44:19 -0300

    fix image urls

--- src/components/Tips/Tips.jsx
+++ src/components/Tips/Tips.jsx
@@ -76,7 +76,7 @@ const Tips = () => {
     return (
         <section className="xl:w-[70dvw] mx-auto px-6 flex flex-col items-center pt-16 relative pb-[100px] mb-20">
             <p className="text-5xl uppercase text-pia-blue-900 w-full font-serif text-center pb-8 font-bold">{strings.title}</p>
-            <img src='../images/tips.png' alt='Eucaliptus and lavander' className='w-min object-contain h-[150px] absolute' />
+            <img src='/public/images/tips.png' alt='Eucaliptus and lavander' className='w-min object-contain h-[150px] absolute' />
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 relative top-[70px]">
                 <TipBox title={strings["first-card"].title} icon={faMusic} text={strings["first-card"].text} button={{url: strings["first-card"].button.url, text: strings["first-card"].button.text}} />
                 <TipBox title={strings["second-card"].title} icon={faUserTie} text={strings["second-card"].text} button={{url: strings["second-card"].button.url, text: strings["second-card"].button.text}} />
